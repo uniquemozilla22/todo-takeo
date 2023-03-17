854c24b4d175eb55fb33b0f44043a579324ab865
yogeshtakeo <yogesh@takeo.ai>
2023-03-16 22:58:09 -0400

    day 2 : phase 1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { useEffect, useState } from "react";
 import Listing from "./Components/List.comp.js";
-import MockData from "./data.json";
+
+const MockData = require("./data.json");
 
 // requirements
 
 /**
  *
- * 1. form that will take the input from the users
- * 2. we will be designing the application components
- * 3. we will be using useState and useEffect to render the application correctly
- * 4. we will make a splendid UI
+ * 1. looking at the different api calls that we can make
+ * 2. what kind of library are there to fetch the data
+ * 3. fetching a data and showing it into the console
+ * 4. we will be looking at managing states through redux (bonus)
  *
  */
 
 function App(props) {
   const [data, setData] = useState(null);
+
+  const fetchData = async () => {
+    let dataFetch = await fetch("https://jsonplaceholder.typicode.com/todos");
+    dataFetch = await dataFetch.json();
+    console.log(dataFetch);
+    setData(dataFetch);
+  };
 
   const listing = () => {
     if (!data) return <h1>Loading...</h1>;
@@ -24,11 +32,11 @@ function App(props) {
     });
   };
 
-  useEffect(() => {
-    setTimeout(() => {
-      setData(MockData);
-    }, 1000);
-  });
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setData(MockData);
+  //   }, 1000);
+  // });
 
   // HTML tags
   // <div></div>
@@ -36,7 +44,12 @@ function App(props) {
 
   // JSX Tags are defined by you specially
 
-  return <div className="App">{listing()}</div>;
+  return (
+    <div className="App">
+      <button onClick={() => fetchData()}>Fetch Data</button>
+      {listing()}
+    </div>
+  );
 }
 
 export default App;
--- src/Components/List.comp.js
+++ src/Components/List.comp.js
@@ -3,8 +3,8 @@ import React from "react";
 const Listing = (yogesh) => {
   return (
     <div>
-      <h1>{yogesh.list.name}</h1>
-      <p>{yogesh.list.description}</p>
+      <h1>{yogesh.list.title}</h1>
+      {/* <p>{yogesh.list.description}</p> */}
     </div>
   );
 };
