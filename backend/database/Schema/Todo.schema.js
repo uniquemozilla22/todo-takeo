9440a04b7d1d8c51131a4653f1fae46897b4eb83
yogeshtakeo <yogesh@takeo.ai>
2023-03-28 23:35:07 -0400

    Added schemas to the app

---
+++ backend/database/Schema/Todo.schema.js
@@ -0,0 +1,11 @@
+// Create a Schema for todo according to the data below
+/**
+ * 
+ * 
+ *  {
+    id: 3,
+    title: "Task 3",
+    description: "this is the main task 3",
+    completed: true,
+  },
+ */
