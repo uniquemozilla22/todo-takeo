ae8d953694a2139d0b24cc664ff456388672d48f
Sumee Pokharel <sumeepokharel@Sumees-MacBook-Pro.local>
2023-03-31 21:32:50 -0500

    api created

--- backend/database/database_connection.js
+++ backend/database/database_connection.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
+import Users from "./Schema/Users.schema.js";
 
 const connectDatabase = async () => {
   const connection = await mongoose.connect(process.env.DB_CONNECTION_URL);
 
   if (connection) {
     console.log("database connected");
+    const sumitrapokharel = new Users({
+      username: "Sumitrapokharel",
+      password: "Sumitra1234",
+    });
+    await sumitrapokharel.save();
   } else {
     console.log("databasse not connected");
   }
