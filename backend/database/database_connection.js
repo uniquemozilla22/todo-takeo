d9c2d421a538264257750fa2bd119b3b4cfb13c1
SarojCode1 <122462571+SarojCode1@users.noreply.github.com>
2023-04-06 13:43:43 -0400

    username

--- backend/database/database_connection.js
+++ backend/database/database_connection.js
@@ -3,16 +3,18 @@ import Users from "./Schema/Users.schema.js";
 
 const connectDatabase = async () => {
   const connection = await mongoose.connect(
-    `mongodb+srv://bhattaraiyogesh007:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`
-  );
+    // `mongodb+srv://bhattaraiyogesh007:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`
 
+    ` mongodb+srv://sarojcode1:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER_NAME}.mongodb.net/?retryWrites=true&w=majority`
+
+  );
   if (connection) {
     console.log("database connected");
-    const sumitrapokharel = new Users({
-      username: "Sumitrapokharel",
-      password: "Sumitra1234",
+    const sarojcode1 = new Users({
+      username: "sarojcode1",
+      password: "12345",
     });
-    await sumitrapokharel.save();
+    await sarojcode1.save();
   } else {
     console.log("databasse not connected");
   }
